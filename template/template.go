1814746d7cdfb1b0644cc2d4bf5b8aaeab2e3531
fbnoi <fbnoi@foxmail.com>
2022-04-10 23:04:24 +0800

    Add template management

--- template/template.go
+++ template/template.go
@@ -1,10 +1,22 @@
 package template
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"sync"
 	"time"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	defaultTemplates = &Templates{
+		cache: make(map[string]*template),
+		lock:  &sync.RWMutex{},
+		up:    make(chan *template),
+	}
 )
 
 type Config struct {
@@ -39,23 +51,86 @@ func (ts *Templates) checkVersion(t *template) {
 	}
 }
 
+func (ts *Templates) hasTemplate(view string) bool {
+	_, ok := defaultTemplates.cache[view]
+	return ok
+}
+
+func (ts *Templates) addTemplate(view string) error {
+
+	t := &template{lock: &sync.Mutex{}}
+	if err := t.parse(view); err != nil {
+		return err
+	}
+	defaultTemplates.lock.Lock()
+	defer defaultTemplates.lock.Unlock()
+
+	defaultTemplates.cache[view] = t
+
+	return nil
+}
+
+func (ts *Templates) getTemplate(view string) *template {
+	defaultTemplates.lock.RLock()
+	defer defaultTemplates.lock.RUnlock()
+
+	if t, ok := ts.cache[view]; ok {
+		return t
+	}
+	return nil
+}
+
 type template struct {
 	name          string
 	tr            *Tree
+	lock          *sync.Mutex
 	lastParseTime time.Time
 }
 
-func (t *template) Parse(view string) {
+func (t *template) parse(view string) error {
+	t.name, t.lastParseTime = view, time.Now()
+	if fh, err := os.Open(view); err == nil {
+		defer fh.Close()
 
+		if content, err := ioutil.ReadAll(fh); err == nil {
+			lex := &Lexer{}
+			stream := lex.Tokenize(&Source{Code: string(content)})
+			filter := &TokenFilter{tr: &Tree{}}
+			t.tr = filter.Filter(stream)
+
+			return nil
+		}
+		return errors.WithStack(err)
+	} else {
+
+		return errors.WithStack(err)
+	}
 }
 
-func (t *template) update() {
-
+func (t *template) update() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	return t.parse(t.name)
 }
 
 func (t *template) Execute(data ...any) []byte {
 	return nil
 }
 
-func Render(w io.Writer, view string, data ...any) {
+func Render(w io.Writer, view string, data ...any) error {
+
+	if !defaultTemplates.hasTemplate(view) {
+		if err := defaultTemplates.addTemplate(view); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+
+	if t := defaultTemplates.getTemplate(view); t != nil {
+		if _, err := w.Write(t.Execute(data)); err != nil {
+			return errors.WithStack(err)
+		}
+		return nil
+	}
+
+	return errors.New(fmt.Sprintf("template %s parsed error", view))
 }
